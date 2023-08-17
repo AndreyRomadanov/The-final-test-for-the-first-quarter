30a44fc77cb968c30a6de7aee8123579a4b1f919
test-github-04 <test-github-04>
2023-08-17 11:43:37 +0500

    Second commit

--- The final test for the first quarter/Program.cs
+++ The final test for the first quarter/Program.cs
@@ -6,35 +6,39 @@
 //["Russia", "Denmark", "Kazan"] → []
 
 
+SamplingArrayElementsByLength();
 
-
-string []array =  {"Hello", "2", "world", ":-)" }  ;
-int j = 0; 
-bool flag = false;
-string[] sorted = new string[array.Length];
-for (int i = 0; i < array.Length; i++)
-{  
-    flag = ConvertArrayElementToArray(array[i],flag);
-    if (flag == true)  
-    {
-        sorted[j] = array[i];
-        j++;
-    }
-}
-PrintArray( sorted);
-
-void PrintArray (string [] elsorted) // Выводим на экран что получилось.
+void SamplingArrayElementsByLength ()
 {
+    string[] array = { "Hello", "2", "world", ":-)","1234", "1567", "-2", "computer science","Russia", "Denmark" };
+    int j = 0;
+    bool flag = false;
+    string[] sorted = new string[array.Length]; // Создаём новый массив.
+    for (int i = 0; i < array.Length; i++) // Проверяем количество символов в элементах массива.
+    {
+        flag = ElementToArray(array[i], flag);
+        if (flag == true)            // Добавляем элементы в новый массив.
+        {
+            sorted[j] = array[i];
+            j++;
+        }
+    }
+    Console.Write("Из заданного массива выбираем строки менее 3 символов , получаем новый массив :");
+    Console.WriteLine();
+   PrintArray(sorted);
+}
+
+void PrintArray (string [] elsorted) // Выводим на экран массив .
+{ 
     for (int i = 0; i < elsorted.Length; i++)
     {
             Console.Write ($" {elsorted[i]} ");       
     }
 }
-
     
-bool ConvertArrayElementToArray (string element , bool elemflag )
+bool ElementToArray (string element , bool elemflag ) 
+// Считаем количество символов в элементе массива , сравниваем с условием задачи.
 {
-    string[] elemarray = new string[element.Length];
     if (element.Length <= 3) elemflag = true;
     else elemflag = false;
     return elemflag;    
