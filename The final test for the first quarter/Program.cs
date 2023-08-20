ce14204347ee8d542b731c850015f33d47e19e19
test-github-04 <test-github-04>
2023-08-20 13:47:40 +0500

    third commit

--- The final test for the first quarter/Program.cs
+++ The final test for the first quarter/Program.cs
@@ -10,11 +10,15 @@ SamplingArrayElementsByLength();
 
 void SamplingArrayElementsByLength ()
 {
-    string[] array = { "Hello", "2", "world", ":-)","1234", "1567", "-2", "computer science","Russia", "Denmark" };
+    int num = 0;
+    string[] array = new string[num];
+    array = ArraySelection(array);
+    Console.WriteLine("Исходный массив  :   ");
+    PrintArray(array);
     int j = 0;
     bool flag = false;
     string[] sorted = new string[array.Length]; // Создаём новый массив.
-    for (int i = 0; i < array.Length; i++) // Проверяем количество символов в элементах массива.
+    for (int i = 0; i < array.Length; i++) // Проверяем количество символов в элементах исходного массива.
     {
         flag = ElementToArray(array[i], flag);
         if (flag == true)            // Добавляем элементы в новый массив.
@@ -23,14 +27,15 @@ void SamplingArrayElementsByLength ()
             j++;
         }
     }
+    Console.WriteLine();
     Console.Write("Из заданного массива выбираем строки менее 3 символов , получаем новый массив :");
     Console.WriteLine();
-   PrintArray(sorted);
+    PrintArray(sorted);
 }
 
 void PrintArray (string [] elsorted) // Выводим на экран массив .
 { 
-    for (int i = 0; i < elsorted.Length; i++)
+    for (int i = 0; i < elsorted.Length ; i++)
     {
             Console.Write ($" {elsorted[i]} ");       
     }
@@ -42,4 +47,33 @@ bool ElementToArray (string element , bool elemflag )
     if (element.Length <= 3) elemflag = true;
     else elemflag = false;
     return elemflag;    
+}
+
+string[] BildSizeArray ( )                 // Задаём параметры массива , вводим массив с клавиатуры.
+{
+Console.Write ( " Введите  количество строк в массиве  :  ") ;
+int realsize = int.Parse ( Console.ReadLine ()! ) ;
+string[] array = new string [realsize ]; 
+for (int i = 0; i < realsize ; i ++ )
+{
+        Console.Write("Введите строку массива  :  ");
+        array[i] = (Console.ReadLine()!);
+}
+return array ;
+}
+
+string[] ReadyMadeArrayInBodyProgram()      // Готовый массив в теле программы.
+{
+        string[] array = { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark" };
+    return array;
+}
+
+string[] ArraySelection (string[] arrayselection)    // Выбираем исходный массив.
+{
+    //int f = 0 ;
+    Console.Write("Эта программа из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна трём символам.Если желаете ввести массив с клавиатуры введите 0, если использовать массив ,встроенный в тело программы , введите любую цифру от 1 до 9.   ");
+    var f = int.Parse(Console.ReadLine()!);
+    if (f == 0) { arrayselection = BildSizeArray(); }
+    else  { arrayselection = ReadyMadeArrayInBodyProgram(); }
+    return (arrayselection);
 }
